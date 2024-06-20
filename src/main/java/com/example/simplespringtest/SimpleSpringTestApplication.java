179bb07b5c098948e50d1c13bd2b086989113a31
faridmustafayev <feridmustafayev34@gmail.com>
2024-06-20 12:16:34 +0400

    SimpleSpringTestApplication added

---
+++ src/main/java/com/example/simplespringtest/SimpleSpringTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.simplespringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringTestApplication.class, args);
+    }
+
+}
