3e076fba65f0c3c9bbf39f2a39834532d7948614
faridmustafayev <feridmustafayev34@gmail.com>
2024-06-20 12:16:46 +0400

    SimpleSpringTestApplicationTests class added

---
+++ src/test/java/com/example/simplespringtest/SimpleSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplespringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
